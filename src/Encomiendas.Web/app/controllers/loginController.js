7e7e35ae63bbbbc6663c06abfabb762233d31a7a
Saul Mayorquin <saul@acklenavenue.com>
2015-07-12 23:28:38 -0600

    cambiando cosas a espanol

--- src/Encomiendas.Web/app/controllers/loginController.js
+++ src/Encomiendas.Web/app/controllers/loginController.js
@@ -1,50 +1,50 @@
-﻿angular.module('Encomiendas.Controllers').controller('loginController', function ($scope, $location, accountService, loginService, userService, facebookService, googleService, menuService) {
-        
+﻿angular.module('Encomiendas.Controllers').controller('loginController', function($scope, $location, accountService, loginService, userService, facebookService, googleService, menuService) {
+
     if (userService.GetUser()) {
         $location.path("/home");
     }
-
+    $scope.rememberMe = true;
     $scope.user = {};
     $scope.$parent.title = "Login";
-    
+
 
     var setUserSession = function(data) {
-        userService.SetUser($scope.user.email, data.name, data.token, $scope.rememberMe, data.expires,data.claims);
+        userService.SetUser($scope.user.email, data.name, data.token, $scope.rememberMe, data.expires, data.claims);
         $scope.$parent.user = userService.GetUser();
         $scope.$parent.menu = menuService.getMenuForUser($scope.$parent.user.claims || []);
         $location.path("/home");
     };
-    
-    $scope.login = function () {
+
+    $scope.login = function() {
         $scope.error = "";
 
-        loginService.Login($scope.user.email, $scope.user.password).then(function (data) {
+        loginService.Login($scope.user.email, $scope.user.password).then(function(data) {
             loginService.SetLoggedIn(true);
             setUserSession(data);
-        }).catch(function (error) {
-            loginService.SetLoggedIn(false);
-            $scope.error = error;
-        });
-    };
-    
-    $scope.loginFacebook = function() {
-        facebookService.Login().then(function (data) {
-            loginService.SetLoggedIn(true);
-            setUserSession(data);
-        }).catch(function (error) {
+        }).catch(function(error) {
             loginService.SetLoggedIn(false);
             $scope.error = error;
         });
     };
 
-    $scope.loginGoogle = function () {
-        googleService.Login().then(function (data) {
+    $scope.loginFacebook = function() {
+        facebookService.Login().then(function(data) {
             loginService.SetLoggedIn(true);
             setUserSession(data);
-        }).catch(function (error) {
+        }).catch(function(error) {
             loginService.SetLoggedIn(false);
             $scope.error = error;
         });
     };
-    
+
+    $scope.loginGoogle = function() {
+        googleService.Login().then(function(data) {
+            loginService.SetLoggedIn(true);
+            setUserSession(data);
+        }).catch(function(error) {
+            loginService.SetLoggedIn(false);
+            $scope.error = error;
+        });
+    };
+
 });
